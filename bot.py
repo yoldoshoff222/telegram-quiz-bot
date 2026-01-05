f5905056200cbe3e23df9d006c6d7a4ffb48a4d4
Sobir <sobir@example.com>
2026-01-05 21:53:40 +0500

    Telegram quiz bot

---
+++ bot.py
@@ -0,0 +1,22 @@
+﻿# -*- coding: utf-8 -*-
+import os
+from telegram.ext import Updater, CommandHandler
+
+TOKEN = os.environ.get("BOT_TOKEN")
+
+def start(update, context):
+    update.message.reply_text(
+        "Assalomu alaykum!\n\n"
+        "/fan1 - 1-fan testlari\n"
+        "/fan2 - 2-fan testlari"
+    )
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
